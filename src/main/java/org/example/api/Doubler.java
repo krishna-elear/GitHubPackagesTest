75b348c2fa022f0174d3ada403d1df58f24fa3e0
krishna-elear <53559967+krishna-elear@users.noreply.github.com>
2021-07-09 13:01:20 +0530

    bla 3

--- src/main/java/org/example/api/Doubler.java
+++ src/main/java/org/example/api/Doubler.java
@@ -1,12 +1,23 @@
 package org.example.api;
 
+/**
+ * A class to double int
+ */
 public class Doubler {
     private final int a;
 
+    /**
+     * Create a doubler instance
+     * @param a The number to double
+     */
     public Doubler(int a) {
         this.a = a;
     }
 
+    /**
+     * Function to double the number provided in constructor
+     * @return The doubled number
+     */
     public int doubleIt() {
         return 2 * a;
     }
